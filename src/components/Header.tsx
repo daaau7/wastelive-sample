ae8688062a4fd6577e3b050340f4cfa9c2a6312e
lrmn7 <lrmn.dev@gmail.com>
2025-08-12 00:52:42 +0700

    change to walletconenct

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import React from 'react';
-
+import "@reown/appkit-wallet-button/react";
 const Header = () => {
   return (
     <header className="w-full flex items-center justify-between px-8 py-4 border-b border-white/10">
     {/*  <div className="text-2xl font-bold">WasteLive</div> */}
      <div></div> 
       <div>
-        <appkit-button />
+      <appkit-wallet-button
+        wallet="walletConnect"
+      />
       </div>
     </header>
   );
