fed0730abd1f1e26b4b2dea75d9a26d468bea037
lrmn7 <lrmn.dev@gmail.com>
2025-08-12 00:57:11 +0700

    change app id config

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,14 @@
 "use client";
 
 import React from 'react';
-import "@reown/appkit-wallet-button/react";
+
 const Header = () => {
   return (
     <header className="w-full flex items-center justify-between px-8 py-4 border-b border-white/10">
     {/*  <div className="text-2xl font-bold">WasteLive</div> */}
      <div></div> 
       <div>
-      <appkit-wallet-button
-        wallet="walletConnect"
-      />
+        <appkit-button />
       </div>
     </header>
   );
